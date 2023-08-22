8718ecb9457adda9511f4470c31bb1f04a4b7d91
youssef oussama <youssama@student.1337.ma>
2023-08-22 08:53:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add other configuration options here if needed
 
-  // Enable static HTML export
-  output: {
-    export: true,
-    output: 'export',
-  },
+
+ const nextConfig = {
+  output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
 
 module.exports = nextConfig;
